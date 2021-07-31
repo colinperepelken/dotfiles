27bba38e8d2efe610d375d96970813c809414144
Colin Perepelken <colin@lingellearning.com>
2021-07-30 21:05:23 -0700

    Create install_macos.sh

---
+++ install_macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install fira code font
+if ! fc-list : file family |grep \/Library grep Fira; then
+  brew tap homebrew/cask-fonts
+  brew cask install font-fira-code
+fi
+# Install spaceship theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
