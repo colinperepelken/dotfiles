83556d65926f7c680273c70987e668d398b26b8a
Colin Perepelken <colin@lingellearning.com>
2021-07-31 20:16:36 -0700

    Update ubuntu install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,5 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt install fonts-firacode
 
 # Install spaceship theme
-curl -o "$ZSH_CUSTOM/themes/spaceship.zsh-theme https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+curl -o ~/.oh-my-zsh-custom/themes/spaceship.zsh-theme https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh
+ln -s ~/.oh-my-zsh-custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh-custom/themes/spaceship.zsh-theme"
