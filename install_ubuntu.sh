61ea3b582f766f227dbf18e7e2171779ffeaf965
Colin Perepelken <colin@perepelken.ca>
2021-07-31 20:29:39 -0700

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,6 +7,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt install fonts-firacode
 
 # Install spaceship theme
-curl -o ~/.oh-my-zsh-custom/themes/spaceship.zsh-theme https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh
 ln -s ~/.oh-my-zsh-custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh-custom/themes/spaceship.zsh-theme
-
