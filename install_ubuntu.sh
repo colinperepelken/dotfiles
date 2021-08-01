8c5e1369a32060225c3e933425017e5cefc10a4c
Colin Perepelken <colin@lingellearning.com>
2021-07-31 20:03:59 -0700

    Add spaceship-theme to install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -7,4 +7,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo apt install fonts-firacode
 
 # Install spaceship theme
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+curl -o - https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh | zsh
