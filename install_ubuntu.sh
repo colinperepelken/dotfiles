542cf97c9876e8268eea1743362fb3817ff1cebc
Colin Perepelken <colin@perepelken.ca>
2021-08-01 21:02:12 -0700

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Install vim
+apt install vim
+
+# Install zsh, and set the shell to zsh
+apt install zsh
+chsh -s $(which zsh)
+
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install fira code font
-sudo apt install fonts-firacode
+apt install fonts-firacode
 
 # Install spaceship theme
 ln -sfn ~/.oh-my-zsh-custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh-custom/themes/spaceship.zsh-theme
